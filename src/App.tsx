193026c52da3b3f1a7e218b5f987c3b6f71360f6
kila <luka.kilasonia.1@btu.edu.ge>
2023-04-12 14:42:35 +0400

    main design with responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 
+import axios  from 'axios';
+import { AxiosResponse } from 'axios'
+
+import { getCityCoordinates, getCityResponseApi } from '../process.env'
+import { useEffect } from 'react';
+ 
+import './App.scss'
+import WeatherPage from './components/weather/weatherPage';
+import SearchPage from './components/search/searchPage';
+
 function App() {
+  let coordinateLat = 0;
+  let coordinateLon = 0;
+
+  // useEffect(() => {
+  //   axios.get(getCityCoordinates('Rustavi'))
+  //   .then((res: AxiosResponse<any, coordinateResponse[]>) => {
+  //     console.log(res.data)
+  //     coordinateLat = res.data[0]?.lat
+  //     coordinateLon = res.data[0]?.lon 
+
+  //     setTimeout(() => {
+  //       axios.get(getCityResponseApi(coordinateLat, coordinateLon)).then(res => {
+  //         console.log(res)
+  //       })
+  //     }, 5000)
+  //   })
+
+  // }, [])
+  
+
 
   return (
-    <div>
-  
+    <div className='page'>
+      <WeatherPage />
+      <SearchPage />
     </div>
   )
 }
