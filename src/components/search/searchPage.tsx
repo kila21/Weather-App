68e9d37b25cebaa48ce060ac662257d8306a9416
kila <luka.kilasonia.1@btu.edu.ge>
2023-04-13 14:31:40 +0400

    add input logic

--- src/components/search/searchPage.tsx
+++ src/components/search/searchPage.tsx
@@ -1,16 +1,36 @@
 
 import './searchPage.scss'
 import location from '../../images/location.svg'
-import { useAppSelector } from '../../store/hooks'
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import { useEffect, useState } from 'react'
+import { getWeatherWithCity } from '../../store/weatherThunk'
 
 
 const SearchPage =  () => {
     const selector = useAppSelector(state => state.weather)
+    const dispatch = useAppDispatch()
+    const [value, setValue] = useState('')
+
+
+    const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value)
+    }
+
+    useEffect(()=> {
+        const timer = setTimeout(()=> {
+            console.log('useffect')
+            console.log(value)
+           dispatch(getWeatherWithCity(value))
+        }, 2000)
+
+        return () => clearTimeout(timer)
+    }, [value])
+
     return (
         <div className='searchPage-container'>
             <div className='searchPage-container-search'>
                 <img src={location} alt='location'/>
-                <input type="text" placeholder='Rustavi'/>
+                <input type="text" placeholder='Rustavi' onChange={inputHandler}/>
             </div>
 
             <div className='searchPage-container-info'>
@@ -19,10 +39,10 @@ const SearchPage =  () => {
                     {selector.humidity ? <p>{selector.humidity} %</p> : ''}
                 </div>
 
-                <div className='searchPage-container-info_content'>
+                {selector.rain ?  <div className='searchPage-container-info_content'>
                     <h4>Chance of Rain</h4>
                     {selector.rain ? <p>{selector.rain} %</p> : ''}
-                </div>
+                </div> : ''}
 
                <div className='searchPage-container-info_content'>
                     <h4>Air Quality</h4>
