82bfca220901bdf3121128daf16335a1e4ba61e8
kila <luka.kilasonia.1@btu.edu.ge>
2023-04-30 12:07:55 +0400

    update api error

--- process.env.ts
+++ process.env.ts
@@ -3,9 +3,11 @@ const getApiKey = () => {
     return 'a7ad3e07634820d796acea406f9b4275'
 }
 
+const ApiGeo = 'http://api.openweathermap.org'
+
 //limit chan be change
 export const getCityCoordinates = (city: string): string => {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=2&appid=${getApiKey()}`
+    return `${ApiGeo}/geo/1.0/direct?q=${city}&limit=2&appid=${getApiKey()}`
     
 }
 
